f48be807efdea014584257791fe9c23c85a161c4
Sebastian-Merlassino <168592380+Sebastian-Merlassino@users.noreply.github.com>
2025-08-07 19:34:18 +0000

    build

--- my-react-app/src/components/db/db.js
+++ my-react-app/src/components/db/db.js
@@ -2,13 +2,22 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID
+  apiKey: "AIzaSyBJ1oc6GQfSdmyASn3bc8dhne2-tKzFHM0",
+  authDomain: "my-ecommerce-fce23.firebaseapp.com",
+  projectId: "my-ecommerce-fce23",
+  storageBucket: "my-ecommerce-fce23.firebasestorage.app",
+  messagingSenderId: "835897976861",
+  appId: "1:835897976861:web:7c7c4d1748fd456054bef4"
 };
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_APP_ID
+// };
 
 
 const app = initializeApp(firebaseConfig);
